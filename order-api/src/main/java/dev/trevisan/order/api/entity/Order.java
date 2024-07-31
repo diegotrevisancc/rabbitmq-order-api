efdcec6d436a40b45893b7879a8709f6a32ac838
Diego Trevisan <diego.trevisan@saamauditoria.com.br>
2024-07-31 14:55:35 -0300

    added order properties

--- order-api/src/main/java/dev/trevisan/order/api/entity/Order.java
+++ order-api/src/main/java/dev/trevisan/order/api/entity/Order.java
@@ -1,9 +1,12 @@
 package dev.trevisan.order.api.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import dev.trevisan.order.api.enums.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,4 +19,7 @@ public class Order {
     private List<OrderItem> items;
     private Double sum;
     private String customerEmail;
+    private Status status;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime time;
 }
