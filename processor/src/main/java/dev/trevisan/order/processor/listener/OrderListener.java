96691fb673aac38235a572e0bfc91bcb9b6c7d70
Diego Trevisan <diego.trevisan@saamauditoria.com.br>
2024-08-01 22:04:39 -0300

    save order

--- processor/src/main/java/dev/trevisan/order/processor/listener/OrderListener.java
+++ processor/src/main/java/dev/trevisan/order/processor/listener/OrderListener.java
@@ -2,6 +2,7 @@ package dev.trevisan.order.processor.listener;
 
 import dev.trevisan.order.processor.entity.Order;
 import dev.trevisan.order.processor.enums.Status;
+import dev.trevisan.order.processor.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -10,11 +11,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderListener {
     private final Logger logger = LoggerFactory.getLogger(OrderListener.class);
+    private final OrderService orderService;
+
+    public OrderListener(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     @RabbitListener(queues = "order.v1.order-created-process-order")
     public void saveOrder(Order order) {
         order.setStatus(Status.PROCESSED);
-
-        logger.info("Order processed {}", order.toString());
+        orderService.save(order);
+        logger.info("Order processed - saved on database {}", order.toString());
     }
 }
